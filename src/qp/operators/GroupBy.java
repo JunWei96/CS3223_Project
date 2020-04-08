ec8481e2d5245e73422a5e9ebd129f80d25489b8
LAPTOP-QAQAFI16\Rohan Dev <rohandev@u.nus.edu>
2020-04-08 18:36:37 +0800

    GroupBy changes -- works for 1 attribute

--- src/qp/operators/GroupBy.java
+++ src/qp/operators/GroupBy.java
@@ -15,18 +15,18 @@ public class GroupBy extends Operator {
 
     private Operator base;                      // The table based on which the GroupBy function is acting on
     private int batchsize;                      // Number of tuples per out batch
-    private int numBuff;
 
     private ArrayList<Attribute> projectlist;   // A list of all the attributes the query wants to project
     private ArrayList<Attribute> groupbylist;   // A list of all the attributes the query wants to group by
 
     private Tuple lastTuple;
-    private int[] attrIndex;
+    private ArrayList<Integer> attrIndex;
 
     ExternalSort sortedOperator;
     ExternalSort.TupleSortComparator comparator;
     int cursor;
     boolean eos;
+    Batch inbatch;
 
 
     public GroupBy(Operator base, ArrayList<Attribute> projectlist, ArrayList<Attribute> groupbylist, int type) {
@@ -47,52 +47,12 @@ public class GroupBy extends Operator {
 
     public void setOperation(Operator base, int numBuff) {
         this.base = base;
-        this.numBuff = numBuff;
         sortedOperator = new ExternalSort(base, numBuff, OpType.GROUPBY);
         comparator = new ExternalSort.TupleSortComparator(sortedOperator.getAttributeList());
     }
 
-    /**
-     ** Returns an int array of Attribute indices; the Group By will group the Operator's tuples in the order
-     *  of the sorted Attribute indices in the array
-     **/
-    private ArrayList<Integer> getSortOrder(ArrayList<Attribute> groupbylist, Schema schema) {
-        /** Initialize the array **/
-        System.out.println("(GroupBy) <getSortOrder>");
-        System.out.println("(GroupBy) <getSortOrder> Schema Number of Columns: " + schema.getNumCols());
-        int[] result = new int[schema.getNumCols()];
-        for (int i = 0; i < schema.getNumCols(); i++) {
-            result[i] = -1;
-        }
-
-        /** Append all the attribute indices of the attributes listed in groupbylist to result array **/
-        int index = 0;
-        for (; index < groupbylist.size(); index++) {
-            Attribute attr = (Attribute) groupbylist.get(index);
-            result[index] = schema.indexOf(attr);
-        }
-
-        /** Append the rest of the attribute indices NOT in groupbylist arbitrarily to result array **/
-        for (int i = 0; i < schema.getNumCols(); i++) {
-            if (result[i] == -1) {
-                result[i] = index;
-                index++;
-            }
-        }
-        System.out.println("(GroupBy) <getSortOrder> Sorted Attribute Order: " + Arrays.toString(result));
-        return (ArrayList<Integer>) Arrays.stream(result) // IntStream
-                                    .boxed()  		// Stream<Integer>
-                                    .collect(Collectors.toList());
-    }
-
     @Override
     public boolean open() {
-        /** select number of tuples per batch **/
-        int tuplesize = schema.getTupleSize();
-
-        /** batchsize = number of records in a page **/
-        this.batchsize = Batch.getPageSize() / tuplesize;
-
         if (!GroupByAttributeValid()) {
             System.out.println("Invalid GroupBy operation");
             return false;
@@ -100,49 +60,41 @@ public class GroupBy extends Operator {
         lastTuple = null;
 
         Schema baseSchema = base.getSchema();
-        attrIndex = new int[projectlist.size()];
+        System.out.println("(GroupBy) <open> baseSchema size: " + baseSchema.getTupleSize());
+        System.out.println("(GroupBy) <open> baseSchema col size: " + baseSchema.getNumCols());
+        attrIndex = new ArrayList<>(projectlist.size());
         for (int i = 0; i < projectlist.size(); i++) {
             Attribute attr = (Attribute) projectlist.get(i);
-            attrIndex[i] = baseSchema.indexOf(attr);
+            System.out.println("(GroupBy) <open> Project List Attribute: " + attr);
+            System.out.println("(GroupBy) <open> Value of i: " + i);
+            System.out.println("(GroupBy) <open> Index of attr: " + baseSchema.indexOf(attr));
+            attrIndex.add(baseSchema.indexOf(attr));
         }
-//        return base.open();
         cursor = 0;
         eos = false;
         System.out.println("(GroupBy) <open> Before sortedOperator is opened");
-
-        if (sortedOperator.open()) {
-            System.out.println("(GroupBy) <open> Check if sortedOperator is opened");
-            Batch inbatch = sortedOperator.next();
-            return true;
-        } else {
-            return false;
-        }
+        return sortedOperator.open();
     }
 
     /**
      * Read the next Batch (i.e. page) from the Operator and read all the Tuples, group them based on specified
      * group by columns, and add them to the outbatch to be written out
-     * **/
+     **/
     @Override
     public Batch next() {
-        Batch inbatch = base.next();
-
+        Batch inbatch = sortedOperator.next();
         if (inbatch == null) {
             return null;
         }
-
         Batch outbatch = new Batch(batchsize);
-
         for (int i = 0; i < inbatch.size(); i++) {
             Tuple basetuple = inbatch.get(i);
             ArrayList<Object> out = new ArrayList<>();
             for (int j = 0; j < projectlist.size(); j++) {
-                out.add((Attribute) basetuple.dataAt(attrIndex[j]));
+                out.add(basetuple.dataAt(attrIndex.get(j)));
             }
             Tuple outtuple = new Tuple(out);
-
-            if (Tuple.compareTuples(outtuple, lastTuple, getCompareOrder(groupbylist, schema),
-                    getCompareOrder(groupbylist, schema)) != 0) {
+            if (lastTuple == null || Tuple.compareTuples(outtuple, lastTuple, attrIndex, attrIndex) != 0) {
                 outbatch.add(outtuple);
             }
             lastTuple = outtuple;
@@ -244,4 +196,37 @@ public class GroupBy extends Operator {
         op.setSchema(this.getSchema());
         return op;
     }
+
+//    /**
+//     ** Returns an int array of Attribute indices; the Group By will group the Operator's tuples in the order
+//     *  of the sorted Attribute indices in the array
+//     **/
+//    private ArrayList<Integer> getSortOrder(ArrayList<Attribute> groupbylist, Schema schema) {
+//        /** Initialize the array **/
+//        System.out.println("(GroupBy) <getSortOrder>");
+//        System.out.println("(GroupBy) <getSortOrder> Schema Number of Columns: " + schema.getNumCols());
+//        int[] result = new int[schema.getNumCols()];
+//        for (int i = 0; i < schema.getNumCols(); i++) {
+//            result[i] = -1;
+//        }
+//
+//        /** Append all the attribute indices of the attributes listed in groupbylist to result array **/
+//        int index = 0;
+//        for (; index < groupbylist.size(); index++) {
+//            Attribute attr = (Attribute) groupbylist.get(index);
+//            result[index] = schema.indexOf(attr);
+//        }
+//
+//        /** Append the rest of the attribute indices NOT in groupbylist arbitrarily to result array **/
+//        for (int i = 0; i < schema.getNumCols(); i++) {
+//            if (result[i] == -1) {
+//                result[i] = index;
+//                index++;
+//            }
+//        }
+//        System.out.println("(GroupBy) <getSortOrder> Sorted Attribute Order: " + Arrays.toString(result));
+//        return (ArrayList<Integer>) Arrays.stream(result) // IntStream
+//                .boxed()  		// Stream<Integer>
+//                .collect(Collectors.toList());
+//    }
 }
